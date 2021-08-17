5591a975a682d9da5859525925431d09a1408890
SH Morad patwary <75469219+SHMorad@users.noreply.github.com>
2021-08-17 22:22:04 +0600

    pioner done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// loginBtn Even Handeler
 var loginBtn = document.getElementById('login')
 loginBtn.addEventListener('click', function(){
 const loginArea = document.getElementById('login-area');
@@ -5,3 +6,40 @@ loginArea.style.display = "none";
 const transactionArea = document.getElementById('transaction-area');
 transactionArea.style.display ='block';
 })
+
+// Deposite button even handeler
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById('depositAmount').value
+    const depositNumber = parseFloat(depositAmount)
+
+    updateSpanText("currentDeposit", depositNumber )
+    updateSpanText("currentBalance", depositNumber)
+
+    document.getElementById('depositAmount').value = ""
+})
+// WithDraw button even handeler
+const WithdrawBtn = document.getElementById('addWithderaw');
+WithdrawBtn.addEventListener('click', function(){
+    const WithdrawNumber = getInputNumber('withdrawAmount')
+    updateSpanText("currentWithdraw", WithdrawNumber)
+    updateSpanText("currentBalance", -1 * WithdrawNumber)
+
+    document.getElementById('withdrawAmount').value =""
+})
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount)
+    return amountNumber;
+}
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currantNumber = parseFloat(current);
+    const totalAmount = depositNumber + currantNumber
+    document.getElementById(id).innerText  = totalAmount;
+}
+
+
+
+
+
